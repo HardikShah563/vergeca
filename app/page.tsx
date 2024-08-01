14418bd69ec61767fd3ba2f56da6388365c1dbaa
Hardik Shah <58135993+HardikShah563@users.noreply.github.com>
2024-08-01 23:54:37 +0530

    adding pages and aos

--- app/page.tsx
+++ app/page.tsx
@@ -5,32 +5,68 @@ import { Features } from "@/components/sections/features";
 import { Pricing } from "@/components/sections/pricing";
 import { Faq } from "@/components/sections/faq";
 import { ContactUs } from "@/components/sections/contact-us";
+import { AOS_Component } from "@/components/aos/aos";
 
 export default function Home() {
+    const aosDelay = 200;
+    const aosDuration = 600;
+
     return (
         <>
             <div className="page">
-                <section>
+                <AOS_Component />
+
+                <section
+                    data-aos="fade-up"
+                    data-aos-anchor-placement="top-bottom"
+                    data-aos-delay={aosDelay}
+                    data-aos-duration={aosDuration}
+                >
                     <Main />
                 </section>
 
-                <section>
+                <section
+                    data-aos="fade-up"
+                    data-aos-anchor-placement="top-bottom"
+                    data-aos-delay={aosDelay}
+                    data-aos-duration={aosDuration}
+                >
                     <About />
                 </section>
 
-                <section>
+                <section
+                    data-aos="fade-up"
+                    data-aos-anchor-placement="top-bottom"
+                    data-aos-delay={aosDelay}
+                    data-aos-duration={aosDuration}
+                >
                     <Features />
                 </section>
 
-                <section>
+                <section
+                    data-aos="fade-up"
+                    data-aos-anchor-placement="top-bottom"
+                    data-aos-delay={aosDelay}
+                    data-aos-duration={aosDuration}
+                >
                     <Pricing />
                 </section>
 
-                <section>
+                <section
+                    data-aos="fade-up"
+                    data-aos-anchor-placement="top-bottom"
+                    data-aos-delay={aosDelay}
+                    data-aos-duration={aosDuration}
+                >
                     <Faq />
                 </section>
 
-                <section>
+                <section
+                    data-aos="fade-up"
+                    data-aos-anchor-placement="top-bottom"
+                    data-aos-delay={aosDelay}
+                    data-aos-duration={aosDuration}
+                >
                     <ContactUs />
                 </section>
             </div>
